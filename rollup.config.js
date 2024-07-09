d7b1c72e33fbe73250cd3ea019e88ae36267b51c
Sugan <sugan@acuitytech.in>
2024-07-09 15:32:56 +0530

    add css

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import { promises as fs } from 'fs';
 import path from 'path';
+import postcss from "rollup-plugin-postcss";
 
 const getPackageJson = async () => {
   const jsonPath = path.resolve('./package.json');
@@ -34,12 +35,14 @@ export default (async () => {
         resolve(),
         commonjs(),
         typescript({ tsconfig: "./tsconfig.json" }),
+        postcss(), 
       ],
     },
     {
       input: "dist/esm/types/index.d.ts",
       output: [{ file: "dist/index.d.ts", format: "esm" }],
       plugins: [dts()],
+      external: [/\.css$/],
     },
   ];
 })();
