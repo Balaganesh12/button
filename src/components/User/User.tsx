7fe16f1237f97ea637bc9f7cf88df66bc541ea4b
Sugan <sugan@acuitytech.in>
2024-07-09 17:04:44 +0530

    add user

---
+++ src/components/User/User.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './User.css'
+ 
+const User = () => {
+  return (
+    <div className="user_container">
+      <div className="user_banner"></div>
+      <div className="user_content"></div>
+    </div>
+  )
+}
+
+export default User
