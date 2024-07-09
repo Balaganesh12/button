1fa3c1cd013d710467c99159286a0f8d77ef7eea
Sugan <sugan@acuitytech.in>
2024-07-09 10:53:45 +0530

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import { promises as fs } from 'fs';
+import path from 'path';
+
+const getPackageJson = async () => {
+  const jsonPath = path.resolve('./package.json');
+  const jsonData = await fs.readFile(jsonPath, 'utf-8');
+  return JSON.parse(jsonData);
+};
+
+export default (async () => {
+  const packageJson = await getPackageJson();
+
+  return [
+    {
+      input: "src/index.ts",
+      output: [
+        {
+          file: packageJson.main,
+          format: "cjs",
+          sourcemap: true,
+        },
+        {
+          file: packageJson.module,
+          format: "esm",
+          sourcemap: true,
+        },
+      ],
+      external: ['tslib'],
+      plugins: [
+        resolve(),
+        commonjs(),
+        typescript({ tsconfig: "./tsconfig.json" }),
+      ],
+    },
+    {
+      input: "dist/esm/types/index.d.ts",
+      output: [{ file: "dist/index.d.ts", format: "esm" }],
+      plugins: [dts()],
+    },
+  ];
+})();
